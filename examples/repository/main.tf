22069571870021b561f41f42fe1f5f2510c41deb
qbart <wojtowicz.bartlomiej@gmail.com>
2023-01-23 12:23:51 +0100

    Update example

--- examples/repository/main.tf
+++ examples/repository/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 module "info" {
   source  = "Selleo/context/null"
   version = "0.3.0"
@@ -11,4 +24,9 @@ module "this" {
   source = "../../modules/repository"
 
   context = module.info.context
+}
+
+output "url" {
+  value = module.this.url
+
 }
